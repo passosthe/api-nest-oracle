26b223df7c1418c00c3e69d9e3a7243c0669f92f
Sthefane Passos Pita <c92407@santacasaba.local>
2025-11-24 14:01:27 -0300

    implementação da lógica

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './users/entities/user.entity';
+import {InjectRepository} from "@nestjs/typeorm";
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private repo: Repository<User>,
+  ) {}
+
+  async create(dto: { name: string; email: string; password: string }): Promise<User> {
+    const hashed = await bcrypt.hash(dto.password, 10);
+    const user = this.repo.create({ ...dto, password: hashed });
+    return this.repo.save(user);
+  }
+
+  findAll() {
+    return this.repo.find();
+  }
+
+  findByEmail(email: string) {
+    return this.repo.findOne({ where: { email } });
+  }
+
+  findById(id: string) {
+    return this.repo.findOne({ where: { id } });
   }
 }
